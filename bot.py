3ebc4a3789cf131c6e10523386f189136611c7dd
TechnocomTnc <predapon.t@gmail.com>
2018-06-18 16:25:41 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,62 +53,12 @@ def handle_message(event):
 
     q = event.message.text.strip(' ')
     print('q=:'+q+':')
-
-    if q.startswith('.'):
-        firstSpace = q.find(' ')
-        if firstSpace > 0:
-            k = q[1:firstSpace]
-            if k:
-                v = q[firstSpace+1:]
-                print('k=:'+k+':')
-                print('v=:'+v+':')
-                queryString = (k)
-                c.execute('SELECT answer FROM chat WHERE question = %s', queryString)
-                fname = c.fetchone()
-                if fname:
-                    queryString = (v, k)
-                    c.execute('UPDATE chat SET answer = %s WHERE question = %s', queryString)
-                    conn.commit()
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text='update...OK'))
-                else:
-                    queryString = (k, v)
-                    c.execute('INSERT INTO chat (question, answer) VALUES (%s, %s)', queryString)
-                    conn.commit()
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text='insert...OK'))
-
-    elif q.startswith('('):
-        inputWord = q[1:]
-        translator = Translator()
-        tsl = translator.translate(inputWord, src='th')
-        print(tsl.text)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=tsl.text))
-
-
-    queryString = ('%'+q+'%')
-    c.execute('SELECT answer FROM chat WHERE question LIKE %s', queryString)
-    a = c.fetchall()
-    if a:
-        print(a)
-        randRow = random.choice(a)
-        print(randRow[0])
-        line_bot_api.reply_message(
-            event.reply_token,
-            # TextSendMessage(text=a[0]))
-            TextSendMessage(text=randRow[0]))
-    else:
-        if queryString.find('อับดุล') > -1:
-            foo = ['เรียกผมเหรอครับ', 'วาจังดายย', 'อะไรวะ', 'เรียกอยู่ได้', 'ถาหาขาไพ่?', 'Zzzz', 'ครับครับ', 'ย๊างหมอ']
-            randAns = random.choice(foo)
-            # print(randAns)
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=randAns))
+    foo = ['เรียกผมเหรอครับ', 'วาจังดายย', 'อะไรวะ', 'เรียกอยู่ได้', 'ถาหาขาไพ่?', 'Zzzz', 'ครับครับ', 'ย๊างหมอ']
+    randAns = random.choice(foo)
+    # print(randAns)
+    line_bot_api.reply_message(
+         event.reply_token,
+         TextSendMessage(text=randAns))
 
     c.close()
     conn.close()
